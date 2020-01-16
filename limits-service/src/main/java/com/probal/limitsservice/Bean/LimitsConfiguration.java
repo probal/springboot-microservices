de920e2984fb92218f8e944194312ae797cafe7e
Probal <probal@cefalo.com>
2020-01-16 14:06:36 +0600

    Spring cloud server added

--- limits-service/src/main/java/com/probal/limitsservice/Bean/LimitsConfiguration.java
+++ limits-service/src/main/java/com/probal/limitsservice/Bean/LimitsConfiguration.java
@@ -7,6 +7,7 @@ public class LimitsConfiguration {
     private int minimum;
 
     public LimitsConfiguration(int maximum, int minimum) {
+        super();
         this.maximum = maximum;
         this.minimum = minimum;
     }
@@ -18,15 +19,8 @@ public class LimitsConfiguration {
         return maximum;
     }
 
-    public void setMaximum(int maximum) {
-        this.maximum = maximum;
-    }
-
     public int getMinimum() {
         return minimum;
     }
 
-    public void setMinimum(int minimum) {
-        this.minimum = minimum;
-    }
 }
--- limits-service/src/main/java/com/probal/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/probal/limitsservice/LimitsConfigurationController.java
@@ -1,14 +1,19 @@
 package com.probal.limitsservice;
 
 import com.probal.limitsservice.Bean.LimitsConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public LimitsConfiguration retrieveLimitsFromConfiguration() {
-        return new LimitsConfiguration(1000,1);
+        LimitsConfiguration limitsConfiguration = new LimitsConfiguration(configuration.getMaximum(),configuration.getMinimum());
+        return limitsConfiguration;
     }
 }

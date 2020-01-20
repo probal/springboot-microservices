ebdd1d6391f1b981e48b015ed57c8d5a8aa6287c
Probal <probal@cefalo.com>
2020-01-20 15:41:13 +0600

    Eureka Service discovery

---
+++ netflix-eureka-naming-server/src/test/java/com/probal/netflixeurekanamingserver/NetflixEurekaNamingServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.probal.netflixeurekanamingserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaNamingServerApplicationTests {
+
+	void contextLoads() {
+	}
+
+}

d079dbf81bd1fafca27e41a1b20636cb402b5f39
Probal <probal@cefalo.com>
2020-01-21 10:26:19 +0600

    Zuul Api gateway implemented

---
+++ netflix-zuul-api-gateway-service/src/test/java/com/probal/netflixzuulapigatewayservice/NetflixZuulApiGatewayServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.probal.netflixzuulapigatewayservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApiGatewayServiceApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
